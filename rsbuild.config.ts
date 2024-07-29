64ba304c6bed81ba0ea312aae1bf75de439a7623
bf-monster <bf.monster@proton.me>
2024-07-29 19:02:04 +0200

    Initial commit

---
+++ rsbuild.config.ts
@@ -0,0 +1,115 @@
+import {defineConfig} from '@rsbuild/core';
+import {pluginReact} from '@rsbuild/plugin-react';
+import {ModuleFederationPlugin} from "@module-federation/enhanced/rspack";
+// @ts-ignore
+import {TanStackRouterRspack} from '@tanstack/router-plugin/rspack'
+// @ts-ignore
+import {dependencies} from "./package.json";
+
+const getStaking = () => {
+	switch (process.env.PUBLIC_ENVIRONMENT) {
+		case 'development':
+			return 'betfinio_staking@https://betfin-staking-dev.web.app/mf-manifest.json'
+		case 'production':
+			return 'betfinio_staking@https://staking.betfin.io/mf-manifest.json'
+		default:
+			return 'betfinio_staking@http://localhost:3000/mf-manifest.json'
+	}
+}
+
+const getApp = () => {
+	switch (process.env.PUBLIC_ENVIRONMENT) {
+		case 'development':
+			return 'betfinio_app@https://betfin-app-dev.web.app/mf-manifest.json'
+		case 'production':
+			return 'betfinio_app@https://betfin-app.web.app/mf-manifest.json'
+		default:
+			return 'betfinio_app@http://localhost:5555/mf-manifest.json'
+	}
+}
+
+export default defineConfig({
+	server: {
+		port: 7777,
+	},
+	dev: {
+		assetPrefix: 'http://localhost:7777',
+	},
+	html: {
+		title: 'BetFin Template',
+		favicon: './src/assets/favicon.svg',
+	},
+	output: {
+		assetPrefix: process.env.PUBLIC_ENVIRONMENT === 'production' ? 'https://betfin-template.web.app' : 'https://betfin-template-dev.web.app'
+	},
+	plugins: [pluginReact()],
+	tools: {
+		rspack: (config, {appendPlugins}) => {
+			config.output!.uniqueName = 'betfinio_template';
+			appendPlugins([
+				TanStackRouterRspack(),
+				new ModuleFederationPlugin({
+					name: 'betfinio_template',
+					remotes: {
+						betfinio_app: getApp(),
+						betfinio_staking: getStaking()
+					},
+					shared: {
+						'react': {
+							singleton: true,
+							requiredVersion: dependencies['react']
+						},
+						'react-dom': {
+							singleton: true,
+							requiredVersion: dependencies['react-dom']
+						},
+						"@tanstack/react-router": {
+							singleton: true,
+							requiredVersion: dependencies['@tanstack/react-router']
+						},
+						"@tanstack/react-query": {
+							singleton: true,
+							requiredVersion: dependencies['@tanstack/react-query']
+						},
+						"@tanstack/react-table": {
+							singleton: true,
+							requiredVersion: dependencies['@tanstack/react-table']
+						},
+						"lucide-react": {
+							singleton: true,
+							requiredVersion: dependencies['lucide-react']
+						},
+						"@supabase/supabase-js": {
+							singleton: true,
+							requiredVersion: dependencies['@supabase/supabase-js']
+						},
+						"i18next": {
+							singleton: true,
+							requiredVersion: dependencies['i18next']
+						},
+						"react-i18next": {
+							singleton: true,
+							requiredVersion: dependencies['react-i18next']
+						},
+						"tailwindcss-animate": {
+							singleton: true,
+							requiredVersion: dependencies['tailwindcss-animate']
+						},
+						"tailwindcss": {
+							singleton: true,
+							requiredVersion: dependencies['tailwindcss']
+						},
+						"wagmi": {
+							singleton: true,
+							requiredVersion: dependencies['wagmi']
+						},
+						"@web3modal/wagmi": {
+							singleton: true,
+							requiredVersion: dependencies['@web3modal/wagmi']
+						}
+					},
+				}),
+			]);
+		},
+	},
+});
