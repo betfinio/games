50a51a1a835636e3e194026b67234a4d8a85c13e
bfmonster <bf.monster@proton.me>
2024-08-07 18:19:10 +0200

    added luro soon

---
+++ src/routes/luro/index.tsx
@@ -0,0 +1,13 @@
+import {createFileRoute, redirect} from '@tanstack/react-router'
+
+export const Route = createFileRoute('/luro/')({
+	component: Luro,
+	beforeLoad: () => {
+		throw redirect({to: '/luro/soon'})
+	}
+})
+
+
+function Luro() {
+	return <div>Soon</div>
+}
