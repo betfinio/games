aa157e4fe66c1c14d38af04cfd466aaaa7f3d506
bfmonster <bf.monster@proton.me>
2024-08-10 00:02:37 +0200

    fixed height on tabs

--- src/components/luro/BetsInfo.tsx
+++ src/components/luro/BetsInfo.tsx
@@ -27,10 +27,10 @@ export const BetsInfo = () => {
 	return (
 		<div className={'flex flex-col h-full'}>
 			<BetsTabs activeTab={activeTab} handleTabClick={handleTabClick} tabs={['Players', 'Bets', 'Bonus']} />
-			<div className={'py-3 mt-3 min-h-[250px] max-h-[500px] overflow-y-auto rounded-md bg-primaryLight grow flex flex-col gap-5 border border-gray-800'}>
-				<div className={'grow flex flex-col '}>
+			<div className={'py-3 mt-3 min-h-[250px] overflow-y-auto rounded-md bg-primaryLight grow flex flex-col gap-5 border border-gray-800'}>
+				<div className={'grow flex flex-col h-full '}>
 					<AnimatePresence mode={'wait'}>
-						<motion.div key={2} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
+						<motion.div className={'h-full'} key={2} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.5 }}>
 							{renderTabContent()}
 						</motion.div>
 					</AnimatePresence>
