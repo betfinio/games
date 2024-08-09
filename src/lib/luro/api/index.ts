a84062f60d6e1db784ef4348679a3072f70b8fa4
bfmonster <bf.monster@proton.me>
2024-08-09 18:42:24 +0200

    added hooks for luro

---
+++ src/lib/luro/api/index.ts
@@ -0,0 +1,236 @@
+import { BETS_MEMORY, LURO, PARTNER } from '@/src/global.ts';
+import type { ICurrentRoundInfo } from '@/src/lib/luro/query';
+import type { LuroBet, PlaceBetParams, Round, RoundStatusEnum } from '@/src/lib/luro/types.ts';
+import { BetsMemoryContract, LotteryBetContract, LotteryContract, PartnerContract, defaultMulticall } from '@betfinio/abi';
+import arrayFrom from '@betfinio/abi/dist/utils';
+import { ZeroAddress } from '@betfinio/hooks';
+import { valueToNumber } from '@betfinio/hooks/dist/utils';
+import { multicall, readContract, writeContract } from '@wagmi/core';
+import { type Address, encodeAbiParameters, parseAbiParameters } from 'viem';
+import { getContractEvents } from 'viem/actions';
+import type { Config } from 'wagmi';
+
+export async function placeBet({ round, amount, player }: PlaceBetParams, config: Config) {
+	try {
+		console.log('PLACING A BET', amount, round);
+		const data = encodeAbiParameters(parseAbiParameters('address player, uint256 amount, uint256 round'), [player, BigInt(amount), BigInt(round)]);
+		return await writeContract(config, {
+			abi: PartnerContract.abi,
+			address: PARTNER,
+			functionName: 'placeBet',
+			value: 0n,
+			args: [LURO, BigInt(amount) * 10n ** 18n, data],
+		});
+	} catch (e) {
+		console.log(e);
+		throw e;
+	}
+}
+
+export const distributeBonus = async ({ round }: { round: number }, config: Config) => {
+	console.log('distributing bonus', round);
+	return await writeContract(config, {
+		abi: LotteryContract.abi,
+		address: LURO,
+		functionName: 'distribute',
+		args: [BigInt(round), 0n, 1000n],
+	});
+};
+
+export const fetchBonusDistribution = async (round: number, config: Config) => {
+	console.log('checking distribution', round);
+	return (await readContract(config, {
+		abi: LotteryContract.abi,
+		address: LURO,
+		functionName: 'roundDistribution',
+		args: [BigInt(round)],
+	})) as boolean;
+};
+
+export const fetchAvailableBonus = async (address: Address, config: Config): Promise<bigint> => {
+	console.log('fetching bonus', address);
+	return (await readContract(config, {
+		abi: LotteryContract.abi,
+		address: LURO,
+		functionName: 'claimableBonus',
+		args: [address],
+	})) as bigint;
+};
+
+export const fetchRoundWinner = async (roundId: number, config: Config): Promise<Round['winner']> => {
+	const logs = await getContractEvents(config.getClient(), {
+		abi: LotteryContract.abi,
+		address: LURO,
+		eventName: 'WinnerCalculated',
+		args: {
+			round: BigInt(roundId),
+		},
+	});
+	if (logs.length === 0) return undefined;
+	const betInfo = (await readContract(config, {
+		abi: LotteryBetContract.abi,
+		address: logs[0].args.bet as Address,
+		functionName: 'getBetInfo',
+	})) as Address[];
+	return {
+		offset: logs[0].args.winnerOffset,
+		bet: logs[0].args.bet,
+		player: betInfo[0],
+		tx: logs[0].transactionHash,
+	};
+};
+
+export const fetchRoundBets = async (roundId: number, config: Config) => {
+	console.trace('fetching round bets', roundId);
+	const count = (await readContract(config, {
+		abi: LotteryContract.abi,
+		address: LURO,
+		functionName: 'getBetsCount',
+		args: [roundId],
+	})) as bigint;
+	const prepared = arrayFrom(Number(count)).map((_, i) => ({
+		abi: LotteryContract.abi,
+		address: LURO,
+		functionName: 'roundBets',
+		args: [roundId, i],
+	}));
+	const result = await multicall(config, {
+		multicallAddress: defaultMulticall,
+		contracts: prepared,
+	});
+	return await Promise.all(result.map((e) => e.result as Address).map((bet) => fetchLotteryBet(bet, config)));
+};
+
+export const fetchLotteryBet = async (address: Address, config: Config): Promise<LuroBet> => {
+	const betInfo = (await readContract(config, {
+		...LotteryBetContract,
+		address: address as Address,
+		functionName: 'getBetInfo',
+	})) as [string, string, bigint, bigint, bigint, bigint];
+	return { player: betInfo[0], amount: betInfo[2], address } as LuroBet;
+};
+
+export async function startRound(round: number, config: Config) {
+	console.log('Starting a round');
+	const data = encodeAbiParameters(parseAbiParameters('uint256 round'), [BigInt(round)]);
+	return await writeContract(config, {
+		abi: LotteryContract.abi,
+		address: LURO,
+		functionName: 'requestCalculation',
+		args: [data],
+	});
+}
+
+export const getCurrentRoundInfo = (iBets: LuroBet[]): ICurrentRoundInfo => {
+	const usersSet = new Set();
+	let volume = 0;
+	const bets = iBets;
+	for (const bet of bets) {
+		usersSet.add(bet.player);
+		volume += valueToNumber(bet.amount);
+	}
+	return {
+		betsCount: bets.length,
+		usersCount: usersSet.size,
+		volume,
+	};
+};
+
+export const fetchRounds = async (player: Address, onlyPlayers: boolean, config: Config): Promise<Round[]> => {
+	console.log('fetching rounds');
+	const activeRounds = await getContractEvents(config.getClient(), {
+		abi: LotteryContract.abi,
+		address: LURO,
+		fromBlock: 9099559n,
+		strict: true,
+		toBlock: 'latest',
+		eventName: 'RoundStart',
+		args: [null, null],
+	});
+	return (await Promise.all(activeRounds.reverse().map((e) => fetchRound(e.args.round, player, config)))).filter((e) => !onlyPlayers || e.player.bets > 0n);
+};
+
+export const fetchRound = async (round: number, player: Address, config: Config): Promise<Round> => {
+	const data = await multicall(config, {
+		multicallAddress: defaultMulticall,
+		contracts: [
+			{
+				abi: LotteryContract.abi,
+				address: LURO,
+				functionName: 'roundBank',
+				args: [round],
+			},
+			{
+				abi: LotteryContract.abi,
+				address: LURO,
+				functionName: 'getBetsCount',
+				args: [round],
+			},
+			{
+				abi: LotteryContract.abi,
+				address: LURO,
+				functionName: 'roundPlayerVolume',
+				args: [round, player],
+			},
+			{
+				abi: LotteryContract.abi,
+				address: LURO,
+				functionName: 'roundPlayerBetsCount',
+				args: [round, player],
+			},
+			{
+				abi: LotteryContract.abi,
+				address: LURO,
+				functionName: 'roundStatus',
+				args: [round],
+			},
+		],
+	});
+	const volume = data[0].result as bigint;
+	const count = data[1].result as bigint;
+	const playerVolume = data[2].result as bigint;
+	const playerCount = data[3].result as bigint;
+	const status = data[4].result as RoundStatusEnum;
+	const winner = await fetchRoundWinner(round, config);
+	const bonus = (volume / 100n) * 4n;
+
+	return {
+		round,
+		total: {
+			volume: volume,
+			bets: count,
+			bonus: bonus,
+			staking: (volume * 360n) / 10000n,
+		},
+		player: {
+			volume: playerVolume,
+			bets: playerCount,
+			bonus: (playerVolume / 100n) * 4n,
+		},
+		status,
+		address: ZeroAddress,
+		winner: winner,
+	};
+};
+
+export const fetchBetsCount = async (config: Config): Promise<number> => {
+	console.log('fetching total bets number');
+	return Number(
+		await readContract(config, {
+			abi: BetsMemoryContract.abi,
+			address: BETS_MEMORY,
+			functionName: 'getGamesBetsCount',
+			args: [LURO],
+		}),
+	);
+};
+
+export const fetchTotalVolume = async (config: Config): Promise<bigint> => {
+	console.log('fetching total volume of lottery');
+	return (await readContract(config, {
+		abi: BetsMemoryContract.abi,
+		address: BETS_MEMORY,
+		functionName: 'gamesVolume',
+		args: [LURO],
+	})) as bigint;
+};
---
+++ src/lib/luro/query/index.ts
@@ -0,0 +1,299 @@
+import { LURO } from '@/src/global.ts';
+import { animateNewBet, getCurrentRound, handleError } from '@/src/lib/luro';
+import {
+	distributeBonus,
+	fetchAvailableBonus,
+	fetchBetsCount,
+	fetchBonusDistribution,
+	fetchRound,
+	fetchRoundBets,
+	fetchRounds,
+	fetchTotalVolume,
+	placeBet,
+	startRound,
+} from '@/src/lib/luro/api';
+import type { LuroBet, PlaceBetParams, Round, WheelState } from '@/src/lib/luro/types.ts';
+import { LotteryContract } from '@betfinio/abi';
+import { ZeroAddress } from '@betfinio/hooks';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { type WriteContractReturnType, readContract } from '@wagmi/core';
+import { useTranslation } from 'react-i18next';
+import type { Address, WriteContractErrorType } from 'viem';
+import { useAccount, useConfig, useWatchContractEvent } from 'wagmi';
+
+export const useObserveBet = () => {
+	const queryClient = useQueryClient();
+	const resetObservedBet = () => {
+		queryClient.setQueryData(['lottery', 'bets', 'newBet'], ZeroAddress);
+	};
+	const query = useQuery<{ address: Address; strength: number }>({
+		queryKey: ['lottery', 'bets', 'newBet'],
+		initialData: { address: ZeroAddress, strength: 0 },
+	});
+
+	return { query, resetObservedBet };
+};
+
+export const usePlaceBet = (address: Address) => {
+	const { t } = useTranslation('', { keyPrefix: 'shared.errors' });
+	const queryClient = useQueryClient();
+	const config = useConfig();
+	return useMutation<WriteContractReturnType, WriteContractErrorType, PlaceBetParams>({
+		mutationKey: ['lottery', 'bets', 'place'],
+		mutationFn: (params) => placeBet(params, config),
+		onError: (e) => {
+			// @ts-ignore
+			console.log(e.cause, t(e.cause?.reason));
+		},
+		onMutate: () => console.log('placeBet'),
+		onSuccess: async (data) => {
+			animateNewBet(address, 10, queryClient);
+			await queryClient.invalidateQueries({ queryKey: ['lottery', 'bets', 'round'] });
+		},
+		onSettled: () => console.log('placeBet settled'),
+	});
+};
+
+export const useStartRound = (round: number) => {
+	const { t } = useTranslation('', { keyPrefix: 'shared.errors' });
+	const { updateState } = useLotteryState();
+	const config = useConfig();
+	useWatchContractEvent({
+		abi: LotteryContract.abi,
+		address: LURO,
+		eventName: 'RequestedCalculation',
+		onLogs: (rolledLogs) => {
+			console.log('ROLLED LOGS', rolledLogs);
+			// @ts-ignore
+			if (Number(rolledLogs[0].args.round) === round) {
+				console.log('START SPINNING');
+				updateState({ state: 'spinning' });
+			}
+		},
+	});
+
+	useWatchContractEvent({
+		abi: LotteryContract.abi,
+		address: LURO,
+		eventName: 'WinnerCalculated',
+		onLogs: (landedLogs) => {
+			console.log('CALCULATED LOGS', landedLogs);
+			// @ts-ignore
+			if (Number(landedLogs[0].args.round) === round) {
+				console.log('LANDED, STOP SPINNING');
+				// @ts-ignore
+				updateState({ state: 'landed', result: Number(landedLogs[0].args.result), bet: landedLogs[0].args.bet });
+			}
+		},
+	});
+
+	return useMutation<WriteContractReturnType, WriteContractErrorType>({
+		mutationKey: ['lottery', 'round', 'start'],
+		mutationFn: () => startRound(round, config),
+		onError: (e) => handleError(e, t),
+		onMutate: () => console.log('Start round'),
+		onSuccess: async (data) => {
+			console.log(data);
+		},
+		onSettled: () => console.log('Round start settled'),
+	});
+};
+
+export const useRoundBets = (round: number) => {
+	const config = useConfig();
+	return useQuery<LuroBet[]>({
+		initialData: [],
+		queryKey: ['lottery', 'bets', 'round', round],
+		queryFn: () => fetchRoundBets(round, config),
+	});
+};
+
+export const useRoundBank = (round: number) => {
+	const config = useConfig();
+	return useQuery<bigint>({
+		initialData: 0n,
+		queryKey: ['lottery', 'bank', 'round', round],
+		queryFn: async () => {
+			return (await readContract(config, {
+				abi: LotteryContract.abi,
+				address: LURO,
+				functionName: 'roundBank',
+				args: [BigInt(round)],
+			})) as bigint;
+		},
+	});
+};
+
+export const useRoundBonusShare = (round: number) => {
+	const config = useConfig();
+
+	return useQuery<bigint>({
+		queryKey: ['lottery', 'bonus', 'round', round],
+		queryFn: async () => {
+			return (await readContract(config, {
+				abi: LotteryContract.abi,
+				address: LURO,
+				functionName: 'roundBonusShares',
+				args: [BigInt(round)],
+			})) as bigint;
+		},
+	});
+};
+
+export const useDistributeBonus = () => {
+	const { t } = useTranslation('', { keyPrefix: 'shared.errors' });
+	const queryClient = useQueryClient();
+	const config = useConfig();
+	return useMutation<WriteContractReturnType, WriteContractErrorType, { round: number }>({
+		mutationKey: ['lottery', 'bonus', 'distribute'],
+		mutationFn: (params) => distributeBonus(params, config),
+		onError: (e) => {
+			console.log(e);
+			handleError(e, t);
+		},
+		onMutate: () => console.log('distribute bonus'),
+		onSuccess: async (data) => {
+			console.log(data);
+			await queryClient.invalidateQueries({ queryKey: ['lottery', 'bonus'] });
+		},
+		onSettled: () => console.log('placeBet settled'),
+	});
+};
+
+export const useBonusDistribution = (round: number) => {
+	const config = useConfig();
+	return useQuery<boolean>({
+		queryKey: ['lottery', 'bonus', 'distribution', round],
+		queryFn: () => fetchBonusDistribution(round, config),
+	});
+};
+
+export const useAvailableBonus = (address: Address) => {
+	const config = useConfig();
+	return useQuery({
+		queryKey: ['lottery', 'bonus', address],
+		queryFn: () => fetchAvailableBonus(address, config),
+	});
+};
+
+export const useRound = (round: number) => {
+	const { address = ZeroAddress } = useAccount();
+	const queryClient = useQueryClient();
+	const config = useConfig();
+	useWatchContractEvent({
+		abi: LotteryContract.abi,
+		address: LURO,
+		eventName: 'BetCreated',
+		args: {
+			round: BigInt(round),
+		},
+		onLogs: async (betLogs) => {
+			console.log('BET LOGS', betLogs);
+			await queryClient.invalidateQueries({ queryKey: ['lottery', 'round', Number(logs[0].args.round)] });
+			if (betLogs[0]?.args?.player !== address) {
+				animateNewBet(betLogs[0]?.args?.player ?? ZeroAddress, 10, queryClient);
+			}
+		},
+	});
+
+	useWatchContractEvent({
+		abi: LotteryContract.abi,
+		address: LURO,
+		eventName: 'WinnerCalculated',
+		onLogs: async (logs) => {
+			console.log('winner', logs[0]);
+			console.log('round', Number(logs[0].args.round));
+			await queryClient.invalidateQueries({ queryKey: ['lottery', 'round', Number(logs[0].args.round)] });
+		},
+	});
+	useWatchContractEvent({
+		abi: LotteryContract.abi,
+		address: LURO,
+		eventName: 'RequestedCalculation',
+		onLogs: (logs) => {
+			console.log('request', logs[0]);
+			// @ts-ignore
+			console.log('round', Number(logs[0].args.round));
+			// @ts-ignore
+			queryClient.invalidateQueries({ queryKey: ['lottery', 'round', Number(logs[0].args.round)] });
+		},
+	});
+	return useQuery<Round>({
+		queryKey: ['lottery', 'round', round],
+		queryFn: () => fetchRound(round, address, config),
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+	});
+};
+
+export const useLotteryState = () => {
+	const queryClient = useQueryClient();
+	const state = useQuery<WheelState>({
+		queryKey: ['lottery', 'state'],
+		initialData: { state: 'standby' },
+	});
+
+	const updateState = (st: WheelState) => {
+		queryClient.setQueryData(['lottery', 'state'], st);
+	};
+
+	return { state, updateState };
+};
+
+export interface ICurrentRoundInfo {
+	betsCount: number;
+	usersCount: number;
+	volume: number;
+}
+
+export const useVisibleRound = () => {
+	const queryClient = useQueryClient();
+	const fetchRound = async (): Promise<number> => {
+		await queryClient.invalidateQueries({ queryKey: ['lottery', 'bets', 'round'] });
+		return getCurrentRound();
+	};
+
+	return useQuery({
+		queryKey: ['lottery', 'visibleRound'],
+		queryFn: fetchRound,
+		initialData: getCurrentRound(),
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+		refetchOnReconnect: false,
+	});
+};
+
+export const useRounds = (player: Address, onlyPlayers = false) => {
+	const config = useConfig();
+	return useQuery<Round[]>({
+		queryKey: ['lottery', 'rounds', player, onlyPlayers],
+		queryFn: () => fetchRounds(player, onlyPlayers, config),
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+	});
+};
+
+export const useRoundInfo = (round: number) => {
+	const config = useConfig();
+	const { address = ZeroAddress } = useAccount();
+	return useQuery<Round>({
+		queryKey: ['lottery', 'round', round],
+		queryFn: () => fetchRound(round, address, config),
+	});
+};
+
+export const useTotalVolume = () => {
+	const config = useConfig();
+
+	return useQuery<bigint>({
+		queryKey: ['lottery', 'totalVolume'],
+		queryFn: () => fetchTotalVolume(config),
+	});
+};
+export const useBetsCount = () => {
+	const config = useConfig();
+	return useQuery<number>({
+		queryKey: ['lottery', 'betsCount'],
+		queryFn: () => fetchBetsCount(config),
+	});
+};
