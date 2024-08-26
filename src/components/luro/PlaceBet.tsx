d70be6cb17290e099226a83e710309f2612b0a8e
bfmonster <bf.monster@proton.me>
2024-08-26 13:00:55 +0200

    added border to place bet

--- src/components/luro/PlaceBet.tsx
+++ src/components/luro/PlaceBet.tsx
@@ -159,7 +159,7 @@ const StandByScreen: FC<{ round: number }> = ({ round }) => {
 				<div className={cx('rounded-xl bg-primaryLight border drop-shadow-[0_0_35px_rgba(87,101,242,0.75)] border-gray-800 p-4 relative w-full')}>
 					<h4 className={'font-medium text-center text-gray-500 text-xs '}>{t('amount')}</h4>
 					<NumericFormat
-						className={'w-full mt-2 rounded-lg text-center text-base lg:text-lg bg-primary py-3 font-semibold text-white disabled:cursor-not-allowed'}
+						className={'w-full mt-2 rounded-lg border border-yellow-400 text-center text-base lg:text-lg bg-primary py-3 font-semibold text-white disabled:cursor-not-allowed'}
 						thousandSeparator={','}
 						min={1}
 						maxLength={15}
@@ -181,7 +181,7 @@ const StandByScreen: FC<{ round: number }> = ({ round }) => {
 						onClick={handleBet}
 						disabled={isPending}
 						className={
-							'text-xs font-semibold flex flex-col items-center justify-center h-[40px] text-center w-full mt-[30px] bg-[#FFC800] rounded-lg min-w-[210px] text-primary'
+							'text-xs font-semibold animate-pulse flex flex-col items-center justify-center h-[40px] text-center w-full mt-[30px] bg-[#FFC800] rounded-lg min-w-[210px] text-primary'
 						}
 					>
 						{isPending ? (
