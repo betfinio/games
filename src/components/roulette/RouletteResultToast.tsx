2c261a2f609bb354bfcc3dbcc0dab356c065e4a2
Sanni <sannikov.matt@proton.me>
2024-08-27 18:09:17 +0200

    Added toast for roulette result

--- src/components/roulette/RouletteResultToast.tsx
+++ src/components/roulette/RouletteResultToast.tsx
@@ -1,53 +1,45 @@
-import { getColor } from "@/src/lib/roulette";
-import { RouletteBet } from "@/src/lib/roulette/types";
-import { valueToNumber } from "@betfinio/abi";
-import { BetLogo } from "@betfinio/ui/dist/icons";
-import { cn } from "betfinio_app/lib/utils";
+import { getColor } from '@/src/lib/roulette';
+import type { RouletteBet } from '@/src/lib/roulette/types';
+import { valueToNumber } from '@betfinio/abi';
+import { BetLogo } from '@betfinio/ui/dist/icons';
+import { cn } from 'betfinio_app/lib/utils';
 
 interface IRouletteResultToastProp {
-  rouletteBet: RouletteBet;
+	rouletteBet: RouletteBet;
 }
 export const RouletteResultToast: React.FC<IRouletteResultToastProp> = ({ rouletteBet }) => {
+	const result = valueToNumber(rouletteBet.result);
+	const winNumber = rouletteBet.winNumber;
 
-  const result = valueToNumber(rouletteBet.result);
-  const winNumber =rouletteBet.winNumber
-  
-
-  return (
-    <div className={"games flex flex-col"}>
-      <div className={"flex flex-col w-full items-center border-b border-gray-800 p-2"}>
-        <span className={"text-sm"}>Winning</span>
-        <div className={"flex flex-row gap-2 justify-center text-lg items-center font-semibold"}>
-          <BetLogo className={"w-4 h-4"} />
-          <span className={cn(rouletteBet.result > 0n ? "text-green-400" : "text-red-500")}>
-            {valueToNumber(rouletteBet.result)}
-          </span>{" "}
-          BET
-        </div>
-      </div>
-      <div className={"w-full flex flex-row items-center"}>
-        <div className={"w-1/2 flex flex-col items-center font-semibold p-2 gap-2 border-r border-gray-800"}>
-          <span className={"text-sm"}>Bet</span>
-          <div className={"flex flex-row gap-2 justify-center text-lg items-center font-semibold"}>
-            <span className={"text-white"}>{valueToNumber(rouletteBet.amount)}</span> BET
-          </div>
-        </div>
-        <div className={"w-1/2 flex flex-col items-center font-semibold p-2 gap-2"}>
-          <span className={"text-sm"}>Win number</span>
-          <span
-            className={cn(
-              "text-white w-8 h-8 rounded-xl flex justify-center whitespace-nowrap font-semibold items-center p-3",
-              {
-                "bg-red-roulette": getColor(winNumber) === "RED",
-                "bg-gray-800": getColor(winNumber) === "BLACK",
-                "bg-green-400": getColor(winNumber) === "GREEN",
-              }
-            )}
-          >
-            {winNumber}
-          </span>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={'games flex flex-col'}>
+			<div className={'flex flex-col w-full items-center border-b border-gray-800 p-2'}>
+				<span className={'text-sm'}>Winning</span>
+				<div className={'flex flex-row gap-2 justify-center text-lg items-center font-semibold'}>
+					<BetLogo className={'w-4 h-4'} />
+					<span className={cn(rouletteBet.result > 0n ? 'text-green-400' : 'text-red-500')}>{valueToNumber(rouletteBet.result)}</span> BET
+				</div>
+			</div>
+			<div className={'w-full flex flex-row items-center'}>
+				<div className={'w-1/2 flex flex-col items-center font-semibold p-2 gap-2 border-r border-gray-800'}>
+					<span className={'text-sm'}>Bet</span>
+					<div className={'flex flex-row gap-2 justify-center text-lg items-center font-semibold'}>
+						<span className={'text-white'}>{valueToNumber(rouletteBet.amount)}</span> BET
+					</div>
+				</div>
+				<div className={'w-1/2 flex flex-col items-center font-semibold p-2 gap-2'}>
+					<span className={'text-sm'}>Win number</span>
+					<span
+						className={cn('text-white w-8 h-8 rounded-xl flex justify-center whitespace-nowrap font-semibold items-center p-3', {
+							'bg-red-roulette': getColor(winNumber) === 'RED',
+							'bg-gray-800': getColor(winNumber) === 'BLACK',
+							'bg-green-400': getColor(winNumber) === 'GREEN',
+						})}
+					>
+						{winNumber}
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 };
--- src/components/roulette/Wheel.tsx
+++ src/components/roulette/Wheel.tsx
@@ -1,152 +1,148 @@
-import { populateRouletteBet } from "@/src/lib/roulette/api";
-import { useRouletteBets, useRouletteState } from "@/src/lib/roulette/query";
-import { ZeroAddress } from "@betfinio/abi";
-import { valueToNumber } from "@betfinio/abi";
-import { useQueryClient } from "@tanstack/react-query";
-import anime from "animejs";
-import { toast, useToast } from "betfinio_app/use-toast";
-import cx from "clsx";
-import { motion } from "framer-motion";
-import { PlayIcon } from "lucide-react";
-import { type FC, useEffect, useState } from "react";
-import type { Address } from "viem";
-import { useAccount, useConfig } from "wagmi";
-import { RouletteResultToast } from "./RouletteResultToast";
+import { populateRouletteBet } from '@/src/lib/roulette/api';
+import { useRouletteBets, useRouletteState } from '@/src/lib/roulette/query';
+import { ZeroAddress } from '@betfinio/abi';
+import { valueToNumber } from '@betfinio/abi';
+import { useQueryClient } from '@tanstack/react-query';
+import anime from 'animejs';
+import { toast, useToast } from 'betfinio_app/use-toast';
+import cx from 'clsx';
+import { motion } from 'framer-motion';
+import { PlayIcon } from 'lucide-react';
+import { type FC, useEffect, useState } from 'react';
+import type { Address } from 'viem';
+import { useAccount, useConfig } from 'wagmi';
+import { RouletteResultToast } from './RouletteResultToast';
 
 const rouletteWheelNumbers = [
-  0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7,
-  28, 12, 35, 3, 26,
+	0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26,
 ];
 
 const RouletteWheel: FC = () => {
-  const { toast, dismiss } = useToast();
-  const { state: wheelStateData, updateState: updateWheelState } = useRouletteState();
-  const { address = ZeroAddress } = useAccount();
-  const { data: bets = [], isFetched: isBetsFetched } = useRouletteBets(address);
-  const lastNumber = bets[0]?.winNumber ?? 0;
-  const config = useConfig();
-  const queryClient = useQueryClient();
-  const wheelState = wheelStateData.data;
-  const singleRotationDegree = 360 / 37;
-  const singleRotationDuration = 5000;
-  const randomOffset = Math.floor(Math.random() * 5 - 2);
-  const [tmp, setTmp] = useState(lastNumber);
+	const { toast, dismiss } = useToast();
+	const { state: wheelStateData, updateState: updateWheelState } = useRouletteState();
+	const { address = ZeroAddress } = useAccount();
+	const { data: bets = [], isFetched: isBetsFetched } = useRouletteBets(address);
+	const lastNumber = bets[0]?.winNumber ?? 0;
+	const config = useConfig();
+	const queryClient = useQueryClient();
+	const wheelState = wheelStateData.data;
+	const singleRotationDegree = 360 / 37;
+	const singleRotationDuration = 5000;
+	const randomOffset = Math.floor(Math.random() * 5 - 2);
+	const [tmp, setTmp] = useState(lastNumber);
 
-  useEffect(() => {
-    if (lastNumber) {
-      setTmp(lastNumber);
-    }
-  }, [lastNumber]);
+	useEffect(() => {
+		if (lastNumber) {
+			setTmp(lastNumber);
+		}
+	}, [lastNumber]);
 
-  const getRouletteIndexFromNumber = (number: number) => {
-    return rouletteWheelNumbers.indexOf(number);
-  };
+	const getRouletteIndexFromNumber = (number: number) => {
+		return rouletteWheelNumbers.indexOf(number);
+	};
 
-  const getRotationFromNumber = (number: number) => {
-    const index = getRouletteIndexFromNumber(number);
-    return singleRotationDegree * index;
-  };
+	const getRotationFromNumber = (number: number) => {
+		const index = getRouletteIndexFromNumber(number);
+		return singleRotationDegree * index;
+	};
 
-  useEffect(() => {
-    if (wheelState.state === "standby") {
-      anime.set([".ROULETTE"], {
-        rotate: () => -getRotationFromNumber(lastNumber),
-      });
-    }
-  }, [isBetsFetched]);
+	useEffect(() => {
+		if (wheelState.state === 'standby') {
+			anime.set(['.ROULETTE'], {
+				rotate: () => -getRotationFromNumber(lastNumber),
+			});
+		}
+	}, [isBetsFetched]);
 
-  useEffect(() => {
-    if (wheelState.state === "spinning") {
-      spinWheel();
-    }
-    if (wheelState.state === "landed") {
-      stopWheel(wheelState.result, wheelState.bet);
-    }
-  }, [wheelState]);
+	useEffect(() => {
+		if (wheelState.state === 'spinning') {
+			spinWheel();
+		}
+		if (wheelState.state === 'landed') {
+			stopWheel(wheelState.result, wheelState.bet);
+		}
+	}, [wheelState]);
 
-  function spinWheel() {
-    if (wheelState.state !== "spinning") return;
+	function spinWheel() {
+		if (wheelState.state !== 'spinning') return;
 
-    anime.set([".ROULETTE"], {
-      rotate: () => -getRotationFromNumber(tmp),
-    });
-    anime({
-      targets: [".ROULETTE"],
-      duration: 2000, // random duration
-      rotate: "-720",
-      easing: "easeInQuad",
-      complete: () => {
-        anime.set([".ROULETTE"], {
-          rotate: () => 0,
-        });
-        anime({
-          targets: [".ROULETTE"],
-          duration: 500, // random duration
-          rotate: "-360",
-          easing: "linear",
-          loop: true,
-        });
-      },
-    });
-  }
+		anime.set(['.ROULETTE'], {
+			rotate: () => -getRotationFromNumber(tmp),
+		});
+		anime({
+			targets: ['.ROULETTE'],
+			duration: 2000, // random duration
+			rotate: '-720',
+			easing: 'easeInQuad',
+			complete: () => {
+				anime.set(['.ROULETTE'], {
+					rotate: () => 0,
+				});
+				anime({
+					targets: ['.ROULETTE'],
+					duration: 500, // random duration
+					rotate: '-360',
+					easing: 'linear',
+					loop: true,
+				});
+			},
+		});
+	}
 
-  async function stopWheel(result: number, bet: Address) {
-    const rouletteBet = await populateRouletteBet(bet, config);
-    const bezier = [0.165, 0.84, 0.44, 1.005];
-    const wheelMinNumberOfSpins = 1;
-    const wheelMaxNumberOfSpins = 2;
+	async function stopWheel(result: number, bet: Address) {
+		const rouletteBet = await populateRouletteBet(bet, config);
+		const bezier = [0.165, 0.84, 0.44, 1.005];
+		const wheelMinNumberOfSpins = 1;
+		const wheelMaxNumberOfSpins = 2;
 
-    const totalRotation = -(
-      Math.floor(Math.random() * (wheelMaxNumberOfSpins - wheelMinNumberOfSpins + 1) + wheelMinNumberOfSpins) * 360 +
-      singleRotationDegree * getRouletteIndexFromNumber(result) +
-      randomOffset
-    );
-    anime.remove(".ROULETTE");
-    anime({
-      targets: [".ROULETTE"],
-      rotate: () => {
-        return totalRotation; // random number
-      },
-      duration: singleRotationDuration, // random duration
-      easing: `cubicBezier(${bezier.join(",")})`,
-      complete: () => {
-        dismiss();
-        toast({
-          component: <RouletteResultToast rouletteBet={rouletteBet} />,
-        });
-        updateWheelState({ state: "standby" });
-        setTmp(result);
-        queryClient.invalidateQueries({ queryKey: ["roulette", "bets"] });
-      },
-    });
-  }
+		const totalRotation = -(
+			Math.floor(Math.random() * (wheelMaxNumberOfSpins - wheelMinNumberOfSpins + 1) + wheelMinNumberOfSpins) * 360 +
+			singleRotationDegree * getRouletteIndexFromNumber(result) +
+			randomOffset
+		);
+		anime.remove('.ROULETTE');
+		anime({
+			targets: ['.ROULETTE'],
+			rotate: () => {
+				return totalRotation; // random number
+			},
+			duration: singleRotationDuration, // random duration
+			easing: `cubicBezier(${bezier.join(',')})`,
+			complete: () => {
+				dismiss();
+				toast({
+					component: <RouletteResultToast rouletteBet={rouletteBet} />,
+				});
+				updateWheelState({ state: 'standby' });
+				setTmp(result);
+				queryClient.invalidateQueries({ queryKey: ['roulette', 'bets'] });
+			},
+		});
+	}
 
-  return (
-    <>
-      <div className={"block"}>
-        <motion.div
-          initial={{ marginTop: "-30%" }}
-          animate={{
-            marginTop: wheelState.state === "spinning" ? "0" : "-30%",
-            paddingTop: wheelState.state === "spinning" ? "60px" : "0",
-            scale: wheelState.state === "spinning" ? 1.15 : 1,
-          }}
-          transition={{ duration: 1 }}
-          className={"flex items-center justify-center overflow-hidden relative pb-20"}
-        >
-          <div
-            className={cx(
-              "rounded-full w-4/5 md:w-2/3 aspect-square lg:drop-shadow-[0_0_35px_rgba(0,172,231,0.45)] bg-roulette-wheel bg-cover ROULETTE",
-              {
-                "blur-md animate-pulse": !isBetsFetched,
-              }
-            )}
-          />
-          <PlayIcon className={"absolute w-5 h-5 text-white z-5 bottom-[72px] rotate-[270deg]"} />
-        </motion.div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className={'block'}>
+				<motion.div
+					initial={{ marginTop: '-30%' }}
+					animate={{
+						marginTop: wheelState.state === 'spinning' ? '0' : '-30%',
+						paddingTop: wheelState.state === 'spinning' ? '60px' : '0',
+						scale: wheelState.state === 'spinning' ? 1.15 : 1,
+					}}
+					transition={{ duration: 1 }}
+					className={'flex items-center justify-center overflow-hidden relative pb-20'}
+				>
+					<div
+						className={cx('rounded-full w-4/5 md:w-2/3 aspect-square lg:drop-shadow-[0_0_35px_rgba(0,172,231,0.45)] bg-roulette-wheel bg-cover ROULETTE', {
+							'blur-md animate-pulse': !isBetsFetched,
+						})}
+					/>
+					<PlayIcon className={'absolute w-5 h-5 text-white z-5 bottom-[72px] rotate-[270deg]'} />
+				</motion.div>
+			</div>
+		</>
+	);
 };
 
 export default RouletteWheel;
