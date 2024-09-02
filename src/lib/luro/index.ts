e86a660db95ba76f6ac827b1bad0d09adfa3026f
bfmonster <bf.monster@proton.me>
2024-09-02 14:33:50 +0200

    added modal switched

--- src/lib/luro/index.ts
+++ src/lib/luro/index.ts
@@ -77,14 +77,20 @@ export const handleError = (e: Error, t: TFunction) => {
 
 export const getLuroInterval = (interval: LuroInterval) => {
 	if (import.meta.env.PUBLIC_ENVIRONMENT.includes('prod')) {
-		return interval === '5m' ? 60 * 5 : 60 * 60 * 24;
+		if (interval === '5m') {
+			return 60 * 5;
+		}
+		return 60 * 60 * 24;
 	}
 	return 60 * 2;
 };
 
 export const getCurrentRound = (interval: LuroInterval) => {
 	if (import.meta.env.PUBLIC_ENVIRONMENT.includes('prod')) {
-		return interval === '5m' ? Math.floor(Date.now() / 1000 / (60 * 5)) : Math.floor((Date.now() + 1000 * 60 * 60 * 6) / 1000 / (60 * 60 * 24));
+		if (interval === '5m') {
+			return Math.floor(Date.now() / 1000 / (60 * 5));
+		}
+		return Math.floor((Date.now() + 1000 * 60 * 60 * 6) / 1000 / (60 * 60 * 24));
 	}
 	return Math.floor(Date.now() / 1000 / (60 * 10));
 };
@@ -92,6 +98,8 @@ export const getCurrentRound = (interval: LuroInterval) => {
 export const getTimesByRound = (round: number, interval: LuroInterval) => {
 	if (import.meta.env.PUBLIC_ENVIRONMENT.includes('prod')) {
 		if (interval === '1d') {
+			const start = round * 60 * 60 * 24 * 1000 - 1000 * 60 * 60 * 6;
+			return { start, end: start + 60 * 60 * 24 * 1000 };
 		}
 		const start = round * 60 * 5 * 1000;
 		return { start, end: start + 60 * 5 * 1000 };
@@ -101,7 +109,7 @@ export const getTimesByRound = (round: number, interval: LuroInterval) => {
 };
 
 export const jumpToCurrentRound = (queryClient: QueryClient) => {
-	queryClient.invalidateQueries({ queryKey: ['luro', 'visibleRound'] });
+	queryClient.invalidateQueries({ queryKey: ['luro'] });
 	queryClient.setQueryData(['luro', 'state'], { state: 'standby' });
 };
 
